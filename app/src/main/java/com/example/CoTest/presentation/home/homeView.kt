a7b0a30b69ee07e6e3b1678d5e779f7d5553fd33
Mohamed Mbarki <Mohamed.mbarki@bitbasegroup.com>
2023-08-06 13:05:54 +0200

    clean code

--- app/src/main/java/com/example/CoTest/presentation/home/homeView.kt
+++ app/src/main/java/com/example/CoTest/presentation/home/homeView.kt
@@ -6,10 +6,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -17,13 +19,18 @@ import androidx.navigation.NavController
 import com.example.domain.loadUseCase.model.UserModel
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.example.CoTest.res.progressBar.CircularIndeterminateProgressBar
@@ -41,6 +48,7 @@ import com.skydoves.landscapist.animation.circular.CircularRevealPlugin
 import com.skydoves.landscapist.coil.CoilImage
 import com.skydoves.landscapist.components.rememberImageComponent
 
+
 @Composable
 fun HomeScreen(
     navController: NavController
@@ -50,9 +58,29 @@ fun HomeScreen(
     val activity = (LocalContext.current as? Activity)
     val progressBar = remember { mutableStateOf(false) }
     val openDialog = remember { mutableStateOf(false) }
+
     val pagedItems: LazyPagingItems<UserModel> =
         viewModel.loadUsesList().collectAsLazyPagingItems()
+    val lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current
 
+    var lifecycleEvent by remember { mutableStateOf(Lifecycle.Event.ON_ANY) }
+    DisposableEffect(lifecycleOwner) {
+        val lifecycleObserver = LifecycleEventObserver { _, event ->
+            lifecycleEvent = event
+        }
+
+        lifecycleOwner.lifecycle.addObserver(lifecycleObserver)
+
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(lifecycleObserver)
+        }
+    }
+
+    LaunchedEffect(lifecycleEvent) {
+        if (lifecycleEvent == Lifecycle.Event.ON_RESUME) {
+            viewModel.fetchNews()
+        }
+    }
     BackHandler(enabled = (currentRoute(navController) == Screen.HomeNav.route)) {
         openDialog.value = true
     }
